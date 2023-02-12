6611409688e81d337b6dc82f799e1c76fd9b48db
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-02-12 20:41:12 +0200

    Change build output dir

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: "./public",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react(), mkcert()],
+  build: {
+    outDir: "build",
+    sourcemap: true,
+    minify: true,
+  },
   server: {
     port: 3399,
     https: true,
