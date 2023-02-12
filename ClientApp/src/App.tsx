d58d0dee10456ff4021abac051cc65bc2ff1363d
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-02-13 01:26:53 +0200

    Remove unused imports

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>Start your cool project from this point</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
